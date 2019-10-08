59983ea89c843d04a61a339829329cbe60dd80bf
nir099 <a.m.s.nirmal.a@gmail.com>
2019-10-08 16:34:36 +0530

    card styles

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -2,31 +2,47 @@ import React, { Component } from 'react'
 import Cards from '../containers/Cards'
 import '../style/certification.css'
 export class Certifications extends Component {
-    cardData = [
-        {
-            id: 1,
-            name: 'Associate',
-            img: 'associate.svg',
-            description: 'Anim deserunt commodo enim est veniam aliquip amet.Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
-        },
-        {
-            id: 2,
-            name: 'Professional',
-            img: 'proffesional.svg',
-            description: 'Anim deserunt commodo enim est veniam aliquip amet. Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
+  constructor(props) {
+    super(props);
+    this.state = { Associate: '' , Professional: '' , Guru: '' };
 
-        },
-        {
-            id: 3,
-            name: 'Guru',
-            img: 'guru.svg',
-            description: 'Anim deserunt commodo enim est veniam aliquip amet. Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
-        }
-    ]
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  handleSubmit(evt) {
+    alert('A name was submitted: ' + this.state[evt.target.name]);
+    evt.preventDefault();
+  }
+  cardData = [
+    {
+      id: 1,
+      name: 'Associate',
+      img: 'associate.svg',
+      description: 'Anim deserunt commodo enim est veniam aliquip amet.Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
+    },
+    {
+      id: 2,
+      name: 'Professional',
+      img: 'proffesional.svg',
+      description: 'Anim deserunt commodo enim est veniam aliquip amet. Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
+
+    },
+    {
+      id: 3,
+      name: 'Guru',
+      img: 'guru.svg',
+      description: 'Anim deserunt commodo enim est veniam aliquip amet. Excepteur eu proident duis nulla commodo in mollit minim. Deserunt veniam veniam qui do sit exercitation. Cillum aliqua ad occaecat incididunt nostrud adipisicing.'
+    }
+  ]
   render() {
     return (
       <div className="container">
-        <Cards data={this.cardData} />
+        <Cards data={this.cardData} handleChange={this.handleChange} handleSubmit={this.handleSubmit} associate={this.state.associate} professional={this.state.professional} guru={this.state.guru} />
       </div>
     )
   }
--- src/containers/Card.js
+++ src/containers/Card.js
@@ -2,17 +2,26 @@ import React from 'react';
 import '../style/certification.css';
 
 export default (props) => {
-    const { data } = props;
+    const { val } = props;
     return (
         <div className="card">
             <div className="w-20 d-flex">
-                    <img src={require(`../assets/img/${data.img}`)} alt={data.img} />
+                <img src={require(`../assets/img/${val.img}`)} alt={val.img} />
             </div>
             <div className="w-80">
-                <h1>{data['name']}</h1>
-                <p className="description">{data.description}</p>
-                <button>Enrol !</button>
+                <h1>{val['name']}</h1>
+                <p className="description">{val.description}</p>
+                <button>Follow Course</button>
+                <form>
+                    <div id='inputForm'>
+                    <label className="inp">
+                        <input type="text" name={val['name']} value={props[val['name']]} onChange={props.handleChange} id="inp" placeholder="Certify Url" />
+                        </label>
+                    </div>
+                        <button className="submitButton" type="submit" name={val['name']} onClick={props.handleSubmit}>Take the Badge</button>
+                </form>
             </div>
-        </div>
-    )
-} 
+            </div>
+            )
+        }
+        
